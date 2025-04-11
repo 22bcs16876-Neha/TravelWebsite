a819d38a39f5f988b4062a8e5de36d413e3fce45
NehaK03 <nehaknandini>
2025-04-12 00:13:36 +0530

    First commit

--- script.js
+++ script.js
@@ -63,28 +63,3 @@ var swiper = new swiper(".review-slider", {
   },
  },
 });
-
-
-
-var swiper = new swiper(".brand-slider", {
- spaceBetween: 20,
- loop:true,
- autoplay: {
-  delay: 2500,
-  disableOnInteraction: false,
- },
- breakpoints: {
-  450: {
-   slidesPerView: 2,
-  },
-  768: {
-   slidesPerView: 3,
-  },
-  991: {
-   slidesPerView: 4,
-  },
-  1200: {
-   slidesPerView: 5,
-  },
- },
-});
